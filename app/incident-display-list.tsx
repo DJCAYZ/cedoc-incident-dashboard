43102b9a0172e87026649f500abf6e989bf54b15
Charles Aaron Sarmiento <charlesaaron07@gmail.com>
2026-06-26 14:56:37 +0800

    [wip] another one

--- app/incident-display-list.tsx
+++ app/incident-display-list.tsx
@@ -12,7 +12,7 @@ export function IncidentDisplayList({ status }: { status: 'active' | 'resolved' 
     });
 
     return (
-        <div className="flex flex-col gap-6 overflow-y-auto h-full pr-2 pb-10 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+        <div className="flex flex-col gap-6 overflow-y-auto h-full pr-2 pb-10 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] scrollbar-none">
             {query.data?.length === 0 && (
                 <div className="flex items-center justify-center h-48 bg-white/50 backdrop-blur-sm rounded-2xl border-2 border-slate-200/50">
                     <p className="text-slate-500 font-bold text-3xl">No {status} incidents.</p>
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,18 +1,44 @@
-export default function SignInPage() {
-    return (
-        <div className="h-screen w-full flex flex-col justify-center items-center">
-            <div className="min-w-[80rem] bg-slate-900/80 backdrop-blur-md shadow-2xl rounded-3xl p-6 border-2 border-slate-700/50 flex flex-col justify-between items-center gap-8 shrink-0">
-                <p className="font-bold text-2xl text-white tracking-tight mb-2">Sign in</p>
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { email, z } from "zod";
 
-                <form className="flex flex-col gap-2 p-6 w-full">
+export default function SignInPage() {
+    const handleSubmit = async (formData: FormData) => {
+        "use server";
+
+        authClient.signIn.email({
+            email: formData.get("email") as string,
+            password: formData.get("password") as string,
+        });
+    }
+
+    return (
+        <div  className="h-screen w-full flex justify-center items-center">
+            <div className="bg-slate-900/80 backdrop-blur-md shadow-2xl rounded-3xl p-6 border-2 border-slate-700/50 flex flex-col justify-between items-center gap-8 shrink-0">
+                <p className="font-bold text-2xl text-white tracking-tight">Sign in</p>
+
+                <form action={handleSubmit} className="flex flex-col gap-2 p-6 w-full">
                     <div className="flex flex-col gap-2">
-                        <label className="text-sm font-medium text-white">Email Address</label>
-                        <input type="text" required className="bg-white border border-slate-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500" />
+                        <label htmlFor="email" className="text-sm font-medium text-white">Email Address</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            className="bg-white border border-slate-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
                     </div>
                     <div className="flex flex-col gap-2">
                         <label className="text-sm font-medium text-white">Password</label>
-                        <input type="password" required className="bg-white border border-slate-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500" />
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            className="bg-white border border-slate-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500"
+                            required
+                        />
                     </div>
+                    <Button type="submit" className="mt-4 bg-blue-600 hover:bg-blue-700 text-white cursor-pointer w-full">Sign in</Button>
                 </form>
             </div>
         </div>
